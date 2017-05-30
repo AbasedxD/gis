fbc50a234ff1199932bbf2f36529e145df9cd104
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-05-29 20:27:41 -0500

    actualizado

--- sbin/8084/contrib/session.lua
+++ sbin/8084/contrib/session.lua
@@ -2,7 +2,7 @@
     Lua 5.1 Copyright (C) 1994-2006 Lua.org, PUC-Rio
 ]]
 
-package.path = ngx.var.app.."/?.lua;"..package.path
+package.path = ngx.var.app.."/8084/?.lua;"..package.path
 local Http = require ("contrib/http");
 local Crypt = require ("contrib/crypt");
 local Sql = require ("contrib/sql");
